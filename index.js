7cacfb69ea73449a59f3549472ffc989391d22b0
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-15 14:17:14 +0300

    add appending cut function to index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+  })
+ // code to destructively add ralph to end of cats array
+  function destructivelyAppendCat (cats) {
+    return cats.push(`Ralph`)
+  }  
+
+  //code to add  bob to beginning of cats array
+  function prependCat(cat) 
+  
